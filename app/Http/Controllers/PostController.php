af266e3ce81102859a0469d84cc1602473b17407
Adil momin <mominadil864@gmail.com>
2021-12-27 16:46:24 +0530

    done till post controller dt:27-12-2021 time:1.27.00

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts=Post::get();  
+        return view('posts.index',[
+            'posts'=>$posts
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'body' =>'required'
+        ]);
+
+        $request->user()->posts()->create($request->only('body'));
+
+
+
+        return back();
+        // Post::create([
+        //     'user_id' => auth()->id(),
+        //     'body'=> $request -> body, 
+        // ]);
+    }  
+}
