488c3fd2d306901bff7b1d3c478407cadc7b8531
Adil momin <mominadil864@gmail.com>
2021-12-28 11:26:36 +0530

    did till like and unlike section added like and unlike functionality on project

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -30,9 +30,26 @@
             <a href="" class="font-bold">{{ $post->user->name }}</a>
             <span class="text-gray-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
             <p class="mb-2">{{ $post->body }}</p>
-        </div>
+            <div class="flex items-center">
+                @if (!$post->likedBy(auth()->user()))
 
+                <form action="{{ route('posts.likes',$post) }}" method="post" class="mr-1">
+                    @csrf
+                    <button type="submit" class="text-blue-500">Like</button>
+                </form>
+                @else
+                 <form action="{{ route('posts.likes',$post) }}" method="post" class="mr-1">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="text-blue-500">Unlike</button>
+                    </form>
+                @endif
+
+                <span>{{ $post->likes->count() }}{{ Str::plural('Like',$post->likes->count()) }}</span>
+            </div>
+        </div>
         @endforeach
+        {{ $posts->links() }}
         @else
         <p>There are no posts</p>
 
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostLikeController;
+
+
 Route::get('/', function(){
     return view('home');
 })->name('home');
@@ -23,4 +26,7 @@ Route::post('/register',[RegisterController::class, 'store']);
 
 Route::get('/posts',[PostController::class, 'index'])->name('posts');
 Route::post('/posts',[PostController::class, 'store']);
- 
+
+Route::post('/posts{post}/likes',[PostLikeController::class, 'store'])->name('posts.likes');
+
+Route::delete('/posts{post}/likes',[PostLikeController::class, 'destroy'])->name('posts.likes');
